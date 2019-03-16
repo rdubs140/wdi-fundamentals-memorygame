a2df6f15ba5c7c21a8deac293ddef2f2eab4e2d2
Ron Wolfe <ronwolfe@Rons-MacBook-Air.local>
2019-03-16 15:26:51 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,25 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo, );
+
+console.log(cardsInPlay);
+console.log("User flipped " + cardsInPlay);
+
+
+
+if (cardsInPlay.length===2); {
+
+} if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You have a match");
+ } else {
+          alert("Sorry, try again");
+ };
